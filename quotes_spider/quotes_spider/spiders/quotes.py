f0462cb1817ce4097c565380f04ce6e3dc341e91
SugiAki <teruaki.sugiura@gmail.com>
2020-05-02 00:06:26 +0900

    save

---
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        # h1_tag = response.xpath("//h1/a/text()").get()
+        # tags = response.xpath('//*[@class="tag-item"]/a/text()').getall()
+        # yield {"H1Tag": h1_tag, "Tags": tags}
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text = quote.xpath('.//*[@class="text"]/text()').get()
+            author = quote.xpath('.//*[@class="author"]/text()').get()
+            tags = quote.xpath('.//*[@class="tag"]/text()').getall()
+
+            yield {
+                "text": text,
+                "author": author,
+                "tags": tags
+            }
+
+        next_page_url = response.xpath('//*[@class="next"]/a/@href').get()
+        abs_next_page_url = response.urljoin(next_page_url)
+
+        yield scrapy.Request(abs_next_page_url)
+
+
+
+
+
